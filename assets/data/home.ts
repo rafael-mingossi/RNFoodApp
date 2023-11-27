758722489e4f620a007b97b930aa0e37845a8ab2
rafael-mingossi <rafaelmingossi@gmail.com>
2023-11-28 09:36:18 +1100

    first commit

---
+++ assets/data/home.ts
@@ -0,0 +1,58 @@
+export const categories = [
+  {
+    text: 'Restaurants',
+    img: require('./c1.png'),
+  },
+  {
+    text: 'Grocery',
+    img: require('./c2.png'),
+  },
+  {
+    text: 'Offers',
+    img: require('./c3.png'),
+  },
+  {
+    text: 'Pickup',
+    img: require('./c4.png'),
+  },
+  {
+    text: 'HOP',
+    img: require('./c5.png'),
+  },
+  {
+    text: 'Pharmacy',
+    img: require('./c6.png'),
+  },
+];
+
+export const restaurants = [
+  {
+    name: 'Vapiano',
+    rating: '4.5 Excellent',
+    ratings: '(500+)',
+    distance: '0.7 miles away',
+    img: require('./r1.jpeg'),
+    tags: ['Italian', 'Pizza', 'Pasta', 'Salads'],
+    duration: '35 - 45',
+  },
+  {
+    name: '✨Urban Greens✨',
+    id: '2',
+    rating: '4.9 Excellent',
+    ratings: '(500+)',
+    distance: '1.7 miles away',
+    img: require('./r2.jpeg'),
+    tags: ['Salads', 'Vegan', 'Healthy', 'British'],
+    duration: '15 - 30',
+  },
+  {
+    name: 'El Minero',
+    id: '3',
+    rating: '4.5 Excellent',
+    ratings: '(500+)',
+    distance: '3 miles away',
+    img: require('./r3.jpeg'),
+    tags: ['Spanish', 'Salads', 'Tpas', 'Pasta'],
+    duration: '25 - 45',
+  },
+];
