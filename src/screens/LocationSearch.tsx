6053f02778241490da8ab7187e3af51c61e81fe9
rafael-mingossi <rafaelmingossi@gmail.com>
2023-12-05 08:43:07 +1100

    Modal, Animated Button Modal

---
+++ src/screens/LocationSearch.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const LocationSearch = () => {
+  return (
+    <SafeAreaView>
+      <Text>OI</Text>
+    </SafeAreaView>
+  );
+};
+
+export default LocationSearch;
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,2 +1,3 @@
 export {default as Home} from './Home';
 export {default as Filter} from './Filter';
+export {default as LocationSearch} from './LocationSearch';
