fba3d978f656d0878fe11cce1b52d9420ad7de02
rafael-mingossi <rafaelmingossi@gmail.com>
2023-12-10 19:51:27 +1100

    imports fix, env imports, map lib

--- babel.config.js
+++ babel.config.js
@@ -3,16 +3,26 @@ module.exports = {
   plugins: [
     'react-native-reanimated/plugin',
     [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+    [
       'module-resolver',
       {
         alias: {
           '@screens': './src/screens',
           '@components': './src/components',
           '@config': './src/config',
-          // '@hooks': './hooks',
           '@utils': './src/utils',
           '@assets': './assets',
           '@constants': './constants',
+          '@env': ['node_modules/react-native-dotenv'],
         },
       },
     ],
--- src/screens/LocationSearch.tsx
+++ src/screens/LocationSearch.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import {REACT_APP_GOOGLE_API_KEY} from '@env';
 
 const LocationSearch = () => {
   return (
     <SafeAreaView>
       <Text>OI</Text>
+      <Text>{REACT_APP_GOOGLE_API_KEY}</Text>
     </SafeAreaView>
   );
 };
