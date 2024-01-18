e24055f52bed85772b5c5bf8426797753e3354f3
rafael-mingossi <rafaelmingossi@gmail.com>
2024-01-18 14:00:11 +1100

    sticky menu section on scroll

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   Dimensions,
   SectionList,
-  ListRenderItem,
   TouchableOpacity,
   Image,
+  ScrollView,
+  SectionListRenderItem,
 } from 'react-native';
 import Animated, {
   interpolate,
@@ -15,6 +16,8 @@ import Animated, {
   useAnimatedRef,
   useAnimatedStyle,
   useScrollViewOffset,
+  useSharedValue,
+  withTiming,
 } from 'react-native-reanimated';
 import {restaurant} from '@assets';
 import {horizontalScale, scaleFontSize, verticalScale} from '@utils';
@@ -34,17 +37,36 @@ type Meals = {
   img: number;
 };
 
+type Item = Meals;
+
+interface Section {
+  title: string;
+  data: Item[];
+  index: number;
+}
+
 const Details = () => {
+  ////USED IN THE PARALLAX SCROLL
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
   scrollOfSet = useScrollViewOffset(scrollRef);
 
+  ////USED IN THE STICKY MENU BAR
+  const scrollRefSticky = useRef<ScrollView>(null);
+  const itemsRef = useRef<TouchableOpacity[]>([]);
+
+  const opacity = useSharedValue(0);
   // const route = useRoute();
   // console.log(route.name);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const DATA = restaurant.food.map((item, index) => ({
     title: item.category,
     data: item.meals,
     index,
+  }));
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    opacity: opacity.value,
   }));
 
   ///USING INTERPOLATE HERE SO WE CAN MAP ONE VALUE TO SOME OTHER VALUE
@@ -73,7 +95,7 @@ const Details = () => {
     return <View style={styles.separator} />;
   };
 
-  const renderListItem: ListRenderItem<Meals> = ({item}) => {
+  const renderListItem: SectionListRenderItem<Meals, Section> = ({item}) => {
     return (
       <TouchableOpacity style={styles.renderItem}>
         <View style={{width: width - verticalScale(110)}}>
@@ -86,9 +108,35 @@ const Details = () => {
     );
   };
 
+  const selectCategory = (index: number) => {
+    ///SELECT THE CURRENT SELECTED BTN
+    const selectedBtn = itemsRef.current[index];
+
+    selectedBtn.measure(x => {
+      scrollRefSticky.current?.scrollTo({
+        x: x - horizontalScale(16),
+        y: 0,
+        animated: true,
+      });
+    });
+
+    ///SETTING INDEX TO HIGHLIGHT SELECTED BTN
+    setActiveIndex(index);
+  };
+
+  const onScroll = (event: any) => {
+    const y = event.nativeEvent.contentOffset.y;
+    if (y > verticalScale(180)) {
+      opacity.value = withTiming(1);
+    } else {
+      opacity.value = withTiming(0);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Animated.ScrollView
+        onScroll={onScroll}
         ref={scrollRef}
         scrollEventThrottle={16}
         showsVerticalScrollIndicator={false}>
@@ -98,7 +146,6 @@ const Details = () => {
         />
         <View
           style={{
-            height: 1100,
             backgroundColor: '#fff',
           }}>
           <Text style={styles.restaurantName}>{restaurant.name}</Text>
@@ -111,6 +158,7 @@ const Details = () => {
           </Text>
           <Text style={styles.restaurantDescription}>{restaurant.about}</Text>
           <SectionList
+            stickySectionHeadersEnabled={false}
             contentContainerStyle={{paddingBottom: 50}}
             keyExtractor={(item, index) => `${item.id + index}`}
             scrollEnabled={false}
@@ -124,6 +172,44 @@ const Details = () => {
           />
         </View>
       </Animated.ScrollView>
+      <Animated.View style={[styles.stickySegment, animatedStyles]}>
+        <View
+          style={{
+            overflow: 'hidden',
+            paddingBottom: 4,
+          }}>
+          <View style={styles.segmentsShadow}>
+            <ScrollView
+              ref={scrollRefSticky}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={styles.segmentScrollView}>
+              {restaurant.food.map((item, index) => (
+                <TouchableOpacity
+                  ref={ref => (itemsRef.current[index] = ref!)}
+                  key={index}
+                  onPress={() => {
+                    selectCategory(index);
+                  }}
+                  style={
+                    activeIndex === index
+                      ? styles.segmentButtonActive
+                      : styles.segmentButton
+                  }>
+                  <Text
+                    style={
+                      activeIndex === index
+                        ? styles.segmentTextActive
+                        : styles.segmentText
+                    }>
+                    {item.category}
+                  </Text>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+          </View>
+        </View>
+      </Animated.View>
     </View>
   );
 };
@@ -187,6 +273,56 @@ const styles = StyleSheet.create({
     fontSize: scaleFontSize(14),
     color: Colors.mediumDark,
     paddingVertical: verticalScale(4),
+  },
+  stickySegment: {
+    position: 'absolute',
+    height: 50,
+    left: 0,
+    right: 0,
+    top: verticalScale(90.05),
+    backgroundColor: '#fff',
+    overflow: 'hidden',
+    paddingBottom: 4,
+  },
+  segmentsShadow: {
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 5,
+    width: '100%',
+    height: '100%',
+  },
+  segmentButton: {
+    paddingHorizontal: horizontalScale(16),
+    paddingVertical: verticalScale(4),
+    borderRadius: 50,
+  },
+  segmentButtonActive: {
+    backgroundColor: Colors.primary,
+    paddingHorizontal: horizontalScale(16),
+    paddingVertical: verticalScale(4),
+    borderRadius: 50,
+  },
+  segmentText: {
+    color: Colors.primary,
+    fontSize: scaleFontSize(16),
+  },
+  segmentTextActive: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: scaleFontSize(16),
+  },
+  segmentScrollView: {
+    paddingHorizontal: horizontalScale(16),
+    alignItems: 'center',
+    gap: 20,
+    paddingBottom: 4,
   },
 });
 
